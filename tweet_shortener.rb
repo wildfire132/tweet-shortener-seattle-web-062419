e8e4e78963f35a1eb0fc2f62757e15a8ff39f0be
Katana Tran <tkatyk21@gmail.com>
2019-06-24 05:29:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,25 +1,17 @@
 # Write your code here.
 
 def dictionary
-"hello" becomes 'hi'
-"to, two, too" become '2' 
-"for, four" become '4'
-'be' becomes 'b'
-'you' becomes 'u'
-"at" becomes "@" 
-"and" becomes "&"
-
 dictionary = {
-  "hello" => "hi"
-  "to" => "2"
-  "two" => "2"
-  "too" => "2"
-  "for" => "4"
-  "four" => "4"
-  "be" => "b"
-  "you" => "u"
-  "at" => "@"
-  "and" => "&"
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&",
 }
 end 
 
