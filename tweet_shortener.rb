bfc100e9291cfaeb5a2cfcf4345080eecb6bfbaf
Katana Tran <tkatyk21@gmail.com>
2019-06-24 04:16:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -10,9 +10,15 @@ def dictionary
 "and" becomes "&"
 
 dictionary = {
-  :hello => "hi"
-  :to => "2"
-  :two => "2"
-  :too => "2"
-  :for =>
+  "hello" => "hi"
+  "to" => "2"
+  "two" => "2"
+  "too" => "2"
+  "for" => "4"
+  "four" => "4"
+  "be" => "b"
+  "you" => "u"
+  "at" => "@"
+  "and" => "&"
+  
 }
