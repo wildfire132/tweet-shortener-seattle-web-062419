a1f7bf2f003d8b76700841dafbcf0734768e6615
Katana Tran <tkatyk21@gmail.com>
2019-06-24 05:57:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,7 +44,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).chars.length > 140  
-    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+    word_substituter(tweet).chars[0..140].push("...").join("")
   else 
     word_substituter(tweet) 
   end 
