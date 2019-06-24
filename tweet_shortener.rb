6db1edfc2ee0104cc9bf2db20641703ba3a45a75
Katana Tran <tkatyk21@gmail.com>
2019-06-24 05:49:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,15 +41,6 @@ elsif tweet.length <= 140
 end
 end 
 
-def shortened_tweet_truncator(tweet)
-  tweet.split(" ").map do |phrase|
-    if phrase.length > 140
-      word_substituter(phrase)[0..140] + "..."
-    else
-      phrase
-    end
-  end.join(" ")
-end
 
 
   
